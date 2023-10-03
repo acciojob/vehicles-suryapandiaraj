3d71b98eaaf34669b6cc522f5102157a94c56db4
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2023-10-03 20:38:19 +0530

    Update Boat.java

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,22 +1,14 @@
-public class Solution {
-    public boolean winnerOfGame(String colors) {
-        Map<Character, Integer> c = new HashMap<>();
-        c.put('A', 0);
-        c.put('B', 0);
-        
-        for (int i = 0; i < colors.length(); i++) {
-            char x = colors.charAt(i);
-            int count = 0;
-            
-            while (i < colors.length() && colors.charAt(i) == x) {
-                i++;
-                count++;
-            }
-            
-            c.put(x, c.get(x) + Math.max(count - 2, 0));
-            i--;
-        }
+package com.driver;
 
-        return c.get('A') > c.get('B');
+public class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+
+    public String getVehicleName() {
+        return name;
+    }
+
+    public int getVehicleCapacity() {
+        return capacity;
     }
 }
