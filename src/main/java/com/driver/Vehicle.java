6678b8775cdfeb4af595903e21d2a9ea06b19dbb
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2023-10-06 04:44:33 +0530

    Update Vehicle.java

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -7,38 +7,48 @@ public class Vehicle {
     private int currentDirection;
 
     public Vehicle(String name) {
-        this.name = name;
-        this.currentSpeed = 0;
-        this.currentDirection = 0;
+        this.name=name;
+        this.currentSpeed=0;
+        this.currentDirection=0;
     }
-
-    public int getCurrentDirection() {
-        return currentDirection;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+
     public int getCurrentSpeed() {
         return currentSpeed;
     }
 
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public int getCurrentDirection() {
+        return this.currentDirection;
+    }
+
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
-        this.currentDirection += direction;
+        setCurrentDirection(getCurrentDirection()+direction);
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
     public void move(int speed, int direction){
         //set the values of currentSpeed and currentDirection
-        this.currentSpeed = speed;
-        this.currentDirection = direction;
+        setCurrentSpeed(speed);
+        setCurrentDirection(direction);
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
-
     public void stop(){
-        this.currentSpeed = 0;
+        setCurrentSpeed(0);
         System.out.println("stop method called - The vehicle is stopped");
     }
 
